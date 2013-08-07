d8111036e1e9cff06ab04958526f76ed3dc6a892
Jason J. Gullickson <jason.gullickson@gmail.com>
2013-08-06 23:32:27 -0500

    added a browser-compatible processing agent

---
+++ minion.js
@@ -0,0 +1,36 @@
+function doBidding(){
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange=function()
+		{
+	  		if (xmlhttp.readyState==4 && xmlhttp.status==200)
+	    	{
+				console.log(xmlhttp.responseText);
+
+				var payload = xmlhttp.responseText;
+
+		    	parsedPayload = JSON.parse(payload);
+
+				data = eval(parsedPayload.data);
+
+				var result = null;
+
+				try{
+					result = eval(parsedPayload.code);
+				}catch(err){
+					result = 'fail';
+				}
+
+				console.log('job output:');
+				console.log(result);
+
+				// callback
+				var cbxmlhttp = new XMLHttpRequest();
+				cbxmlhttp.open("GET",'http://localhost:1337/updatejob?result=' + result,true);
+				cbxmlhttp.send();
+	    	}
+	  	}
+	xmlhttp.open("GET","http://localhost:1337/getjob",true);
+	xmlhttp.send();
+}
+
+doBidding();
