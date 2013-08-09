a177a43ee738418ad7044840673494ca69aedd29
jjg <jason.gullickson@gmail.com>
2013-08-09 00:19:12 -0500

    update

--- server.js
+++ server.js
@@ -61,13 +61,13 @@ http.createServer(function (req, res) {
  	res.writeHead(200, {'Content-Type': 'text/plain','Access-Control-Allow-Origin': '*'});
  	res.end(payload);
 
-}).listen(1337, '10.0.1.15');
+}).listen(1337, '127.0.0.1');
 
-console.log('Server running at http://10.0.1.15:1337/');
+console.log('Server running at http://127.0.0.1:1337/');
 
 function getJob(nodeId){
 
-	var payload = "{\"nodeId\":\"" + nodeId + "\",\"callback\":\"\'http://\'+ strawboss + \':1337/status?nodeid=\' + nodeId + \'&capacity=\' + result\",\"code\":\"function getFlops(){var s = new Date().getTime(); for(i=0;i<10000;i++){m = 10 / 3.3;} var e = new Date().getTime(); d = e - s;  return d;} getFlops();\", \"data\":{}}"
+	var payload = "{\"nodeId\":\"" + nodeId + "\",\"callback\":\"\'http://\'+ strawboss + \'/status?nodeid=\' + nodeId + \'&capacity=\' + result\",\"code\":\"function getFlops(){var s = new Date().getTime(); for(i=0;i<10000;i++){m = 10 / 3.3;} var e = new Date().getTime(); d = e - s;  return d;} getFlops();\", \"data\":{}}"
 	//var payload = "{\"code\": \"function sayHello(who){var helloPhrase = \'hello \' + who;return helloPhrase} sayHello(data.username);\",\"data\":{\"username\": \"jason\"}}";
 	//var payload = "{\"code\": \"function stealPasswd(){fs.readFile(\'/etc/passwd\',function(e,t){return t})} stealPasswd();\",\"data\":{}}";
 
